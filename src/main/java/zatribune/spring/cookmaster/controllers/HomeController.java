e93bf332a7c81797bead0a6dced8f6743fb60ae3
Muhammad Ali <muhammadali40k@gmail.com>
2021-01-07 06:01:01 +0200

    extra ui modifications

--- src/main/java/zatribune/spring/cookmaster/controllers/HomeController.java
+++ src/main/java/zatribune/spring/cookmaster/controllers/HomeController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-
    @RequestMapping("/home")
    public String getHomePage(Model model){
 
